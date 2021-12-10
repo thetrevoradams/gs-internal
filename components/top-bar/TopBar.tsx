3f548599f5a960a5071beb3cc9956380575aa6da
Trevor Adams <contact@thetrevoradams.com>
2021-12-09 23:10:11 -0700

    updating placeholder

--- components/top-bar/TopBar.tsx
+++ components/top-bar/TopBar.tsx
@@ -9,7 +9,7 @@ const TopBar = () => {
     <div className="bg-white p-5 w-full border-b border-gray-light flex content-end">
       <div className="flex-grow flex content-end">
         <Search className="text-gray" />
-        <span>input here</span>
+        <span> [Put input here]</span>
       </div>
       <Bell className="text-gray hover:text-blue mr-2" />
       <button onClick={handleSignOut}>
